4f21a70a097269a7765abfa46700d50317ab5692
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2025-07-17 00:51:15 +0100

    ontology generation and results page

--- app/app.py
+++ app/app.py
@@ -1,8 +1,26 @@
-from flask import Flask, render_template, jsonify, request, redirect
+from flask import Flask, render_template, jsonify, request, redirect, session, url_for, abort
 import requests
+import os
+import secrets
+from dotenv import load_dotenv
 from datetime import datetime
+import cocktail_scraper.dependencies_parser
+from cocktail_scraper.scraper import scrap_data
+from cocktail_scraper.data_processor import process_data
+from cocktail_scraper.translator import generate_cic
+from pprint import pprint
+from flask_caching import Cache
+
+load_dotenv() 
 
 app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+
+cache = Cache(config={
+    'CACHE_TYPE': 'SimpleCache',
+    'CACHE_THRESHOLD': 100, # Max size
+    'CACHE_DEFAULT_TIMEOUT': 3600}) # Stores for 1 hour
+cache.init_app(app)
 
 #render_template('homepage.html', date={"date": formated_iso_date})
 
@@ -17,7 +35,74 @@ def homepage():
 
 @app.route('/about', methods=['GET'])
 def about():
-    return render_template('about.html')  # make sure you have about.html
+    return render_template('about.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    # Get form data
+    repo_url = request.form.get('repo_url')
+    github_token = request.form.get('github_token')
+
+    session['github_token'] = github_token
+    
+    # Validate inputs
+    if not repo_url or not github_token:
+        return "Missing required fields", 400
+    
+    # Process data
+    try:
+        results = generate_repo_cic(repo_url, github_token)
+        # Generate unique ID and store results
+        result_id = secrets.token_hex(8)
+        cache.set(result_id, results)
+        
+        # Redirect with ID instead of full data
+        return redirect(url_for('results', result_id=result_id))
+    
+    except Exception as e:
+        print(f"Error generating results: {str(e)}")
+        return "Error processing request", 500
+
+@app.route('/results')
+def results():
+    result_id = request.args.get('result_id')
+    results = cache.get(result_id) if result_id else None
+
+    if not results:
+        # Redirect if no results
+        return redirect(url_for('index'))
+    
+    try:
+        return render_template('results.html', 
+                           data={"cocktail": results["cic_data"]})
+        
+    except Exception as e:
+        print(f"Error loading results: {str(e)}")
+        return redirect(url_for('homepage'))
+
+def generate_repo_cic(repo_url, github_token):
+    repo_data = scrap_data(repo_url,github_token)
+    print("SCRAPER:\n")
+    print(repo_data)
+
+    processed_data = process_data(repo_data)
+    print("PROCESSOR:\n")
+    print(processed_data)
+
+    if len(processed_data) == 1:
+        cic = generate_cic(processed_data[0])
+        print("TRANSLATOR:\n")
+        print(cic)
+    else:
+        print("Error: Fetched multiple repos somehow")
+
+    results = {
+        "scraper_data" : repo_data,
+        "processor_data" : processed_data,
+        "cic_data" : cic 
+    }
+
+    return results
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
---
+++ app/cocktail_scraper/translator.py
@@ -0,0 +1,265 @@
+import json
+from .graph_gen.odlc import generate_graph
+
+# for identation
+small_tab = "        "
+big_tab = "                "
+
+# Nested dictionary cases
+nested_cases = ['optional','groups','target']
+
+"""
+def list_to_tuple(obj):
+    if isinstance(obj, list):
+        return tuple(list_to_tuple(x) for x in obj)
+    return obj
+
+def restore_tuples(repo):
+    if "ingredients" in repo and "dependencies" in repo["ingredients"]:    
+            restoration = repo["ingredients"]    
+            for type in restoration["dependencies"]:
+                if type in nested_cases:
+                    for sub in restoration["dependencies"][type]:
+                        restoration["dependencies"][type][sub] = list_to_tuple(restoration["dependencies"][type][sub])
+                else:
+                    restoration["dependencies"][type] = list_to_tuple(restoration["dependencies"][type])
+            
+            repo["ingredients"] = restoration
+    
+    return repo
+"""
+
+# Creates tuples from a list of ingredients found in dependency files
+def dependency_tuples(list,already_used,app_name,cocktail_name):
+    ontology = ""
+
+    for ingredient in list:
+        if ingredient["name"] not in already_used:
+            # Resource triples
+            if ingredient["type"] == "Resource":
+                ontology += big_tab + ingredient["name"] + " = iof => Resource;\n"
+                ontology += big_tab + ingredient["name"] + " = supports => " + app_name + ";\n"
+            else:
+                # Remaining triples
+                ontology += big_tab + ingredient["name"] + " = iof => " + ingredient["type"] + ";\n"
+                ontology += big_tab + ingredient["name"] + " = pof => " + cocktail_name + ";\n"
+                
+                if "associated_languages" in ingredient:
+                    # Library
+                    if ingredient["type"] == "Library":
+                        for lang in ingredient["associated_languages"]:
+                            ontology += big_tab + ingredient["name"] + " = extends => " + lang + ";\n"
+                    # Framework
+                    elif ingredient["type"] == "Framework":
+                        for lang in ingredient["associated_languages"]:
+                            ontology += big_tab + ingredient["name"] + " = encloses => " + lang + ";\n"       
+                    # Tool
+                    else:
+                        for lang in ingredient["associated_languages"]:
+                            ontology += big_tab + ingredient["name"] + " = supports => " + lang + ";\n"
+                
+                if "associated_tasks" in ingredient:
+                    for task in ingredient["associated_tasks"]:
+                        ontology += big_tab + ingredient["name"] + " = is_used_for => " + task + ";\n"
+        
+        already_used.add(ingredient["name"])
+
+    return (ontology,already_used)
+            
+# Uses the repository data to build the Cocktail Ontology
+def translate_data(repo_data):
+    already_used = set()
+
+    # Set names of entities to be used many times in the ontology
+    app_name = repo_data["name"]
+    dev_name = repo_data["name"] + "Development"
+    cocktail_name = repo_data["name"] + "Cocktail"
+
+    ontology = """  Ontology OntoCoq
+
+        concepts {
+                System,
+                Resource,
+                Development,
+                Task,
+                Cocktail,
+                Ingredient,
+                Language,
+                Library,
+                Framework,
+                Tool
+
+    """
+    # Tasks
+        # if tasks ontology += ",/n% Tasks:"
+    ontology += """
+        }
+
+        % Individuals
+        individuals {
+"""
+    # Individuals
+    # Instantiate the app, its development and its cocktail
+    ontology += big_tab + app_name + ",\n" + big_tab + dev_name + ",\n" + big_tab + cocktail_name
+
+    # Instantiate all languages
+    if "languages" in repo_data:
+        for lang in repo_data["languages"]:
+                ontology +=  ",\n" + big_tab + lang
+
+    # Instantiate all ingredients   
+    if "ingredients" in repo_data and "dependencies" in repo_data["ingredients"]:
+        for dep_type, dep_value in repo_data["ingredients"]["dependencies"].items():
+            if dep_type != "target":
+                if dep_type in nested_cases:
+                    # dep_value is a dict of sub-dependencies
+                    for sub in dep_value:
+                        for ingredient in dep_value[sub]:
+                            if ingredient["name"] not in already_used:
+                                ontology += ",\n" + big_tab + ingredient["name"]
+                                already_used.add(ingredient["name"])
+                else:
+                    # dep_value is a list of ingredients
+                    for ingredient in dep_value:
+                        if ingredient["name"] not in already_used:
+                            ontology += ",\n" + big_tab + ingredient["name"]
+                            already_used.add(ingredient["name"])
+            else:
+                # 'target' case for cargo.toml are more nested than usual, thus special treatment
+                for target in dep_value:
+                        # sub can be 'necessary', 'devDependency', etc...
+                        for sub in dep_value[target]:
+                            for ingredient in dep_value[target][sub]:
+                                if ingredient["name"] not in already_used:
+                                    ontology += ",\n" + big_tab + ingredient["name"]
+                                    already_used.add(ingredient["name"])
+
+
+    ontology += """
+        }
+
+        relationships {
+                uses,
+                contains,
+                requires,
+                supports,
+                is_used_for,
+                extends,
+                encloses
+        }
+
+        triples {
+                % Conceptual plan:
+                System = requires => Development;
+                Resource = supports => System;
+                Development = uses => Cocktail;
+
+                Task = pof => Development;
+                Ingredient = pof => Cocktail;
+                Ingredient = is_used_for => Task;
+
+                Language = isa => Ingredient;
+                Library = isa => Ingredient;
+                Framework = isa => Ingredient;
+                Tool = isa => Ingredient;
+                Library = extends => Language;
+                Framework = encloses => Language;
+                Tool = supports => Language;  
+"""
+    # Triples
+    # System triples
+    ontology += big_tab + app_name + " = iof => System;\n"
+    ontology += big_tab + dev_name + " = iof => Development;\n"
+    ontology += big_tab + cocktail_name + " = iof => Cocktail;\n"
+
+    ontology += big_tab + app_name + " = requires => " + dev_name + ";\n"
+    ontology += big_tab + dev_name + " = uses => " + cocktail_name + ";\n"
+
+    # Task triples
+    if "ingredients" in repo_data and "tasks" in repo_data["ingredients"]:
+        for task in repo_data["ingredients"]["tasks"]:
+            ontology += big_tab + task + " = isa => Task;\n"
+            ontology += big_tab + task + " = pof => " + dev_name + ";\n"
+
+    already_used.clear()
+
+    # Ingredient triples
+    #   Language triples
+    if "languages" in repo_data:
+        for lang in repo_data["languages"]:
+                ontology += big_tab + lang + " = iof => Language;\n"
+                ontology += big_tab + lang + " = pof => " + cocktail_name +";\n"
+                # is_used_for => Task
+
+    if "ingredients" in repo_data:
+        # Dependency ingredient tuples
+        if "dependencies" in repo_data["ingredients"]:
+            for dep_type, dep_value in repo_data["ingredients"]["dependencies"].items():
+                if dep_type != "target":
+                    if dep_type in nested_cases:
+                        # dep_value is a dict of sub-dependencies
+                        for sub in dep_value:
+                            result = dependency_tuples(dep_value[sub],already_used,app_name,cocktail_name)
+                            # Update ontology with result
+                            ontology += result[0]
+                            # Update set of used names
+                            already_used = result[1]
+                    else:
+                        result = dependency_tuples(dep_value,already_used,app_name,cocktail_name)
+                        ontology += result[0]
+                        already_used = result[1]
+                else:
+                    # 'target' case for cargo.toml are more nested than usual, thus special treatment
+                    for target in dep_value:
+                            # sub can be 'necessary', 'devDependency', etc...
+                            for sub in dep_value[target]:
+                                result = dependency_tuples(dep_value[target][sub],already_used,app_name,cocktail_name)
+                                ontology += result[0]
+                                already_used = result[1]
+
+    ontology += """
+    }
+.
+"""
+
+    return ontology
+
+# Generates a repository's ontology and Cocktail Identity Card
+def generate_cic(repo_data):
+    ontology = translate_data(repo_data)
+    result = {
+        "name": repo_data["name"],
+        "ontology": ontology,
+        "cic": "TBD",
+        "graph": "TBD"
+    }
+
+    return result
+
+# Makes ontologies from data loaded from a JSON file, generates the corresponding graph and saves them localy (for debug, requires empty "generated_ontologies" folder)
+def test_ontology():
+    ontology_data = []
+
+    with open("processed_data.json") as f:
+        data = json.load(f)
+        for repo in data:
+            print(repo["name"])
+            ontology = translate_data(repo)
+            ontology_data.append({"name":repo["name"],"ontology":ontology})
+            filename = "generated_ontologies/" + repo["name"] + "_ontology.txt"
+
+            # Write the data to the file
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(ontology)
+            
+            generate_graph(repo["name"],ontology,True)
+            
+
+    # File handling
+    filename = "ontology_data.json"
+
+    # Write the updated data back to the file
+    with open(filename, 'w') as f:
+        json.dump(ontology_data, f, indent=4)  
+
+#test_ontology()
